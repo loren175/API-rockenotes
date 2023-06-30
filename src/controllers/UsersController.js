8fecfd91c7c626f54af07aaa65a20ba1de1e2c30
Rafael Loren175 <rafael.loren175@gmail.com>
2023-06-30 13:24:52 -0300

    add sql and user register function

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,25 @@
 const AppError = require("../utils/AppError")
+const sqliteConnection = require("../database/sqlite")
 
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    )
 
-    if (!name) {
-      throw new AppError("Nome é obrigatório!")
+    if (checkUserExists) {
+      throw new AppError("Este email já está em uso!")
     }
 
-    response.status(201).json({ name, email, password })
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, password]
+    )
+
+    return response.status(201).json()
   }
 }
 
