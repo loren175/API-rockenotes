eaf575b7cb4eed6dda0e0b803b7848982fa7d201
Rafael Loren175 <rafael.loren175@gmail.com>
2023-07-01 12:15:46 -0300

    add put method and cryptography to passwords

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,4 @@
+const { hash } = require("bcryptjs")
 const AppError = require("../utils/AppError")
 const sqliteConnection = require("../database/sqlite")
 
@@ -14,12 +15,50 @@ class UsersController {
       throw new AppError("Este email já está em uso!")
     }
 
+    const hashedPassword = await hash(password, 8)
+
     await database.run(
       "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-      [name, email, password]
+      [name, email, hashedPassword]
     )
 
     return response.status(201).json()
+  }
+
+  async update(request, response) {
+    const { name, email } = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado.")
+    }
+    const userWithUpdatedEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    )
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este email já está em uso.")
+    }
+
+    user.name = name
+    user.email = email
+
+    await database.run(
+      `
+    UPDATE users SET
+    name = ?,
+    email = ?,
+    updated_at = ?
+    WHERE id = ?
+    `,
+      [user.name, user.email, new Date(), id]
+    )
+
+    return response.json()
   }
 }
 
