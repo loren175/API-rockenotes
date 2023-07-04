2d3277b967844157e2f807915755d0d6542d16ab
Rafael Loren175 <rafael.loren175@gmail.com>
2023-07-04 14:32:25 -0300

    update datetime and validate function for email and username

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs")
+const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError")
 const sqliteConnection = require("../database/sqlite")
 
@@ -26,7 +26,7 @@ class UsersController {
   }
 
   async update(request, response) {
-    const { name, email } = request.body
+    const { name, email, password, old_password } = request.body
     const { id } = request.params
 
     const database = await sqliteConnection()
@@ -44,18 +44,35 @@ class UsersController {
       throw new AppError("Este email já está em uso.")
     }
 
-    user.name = name
-    user.email = email
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if (password && !old_password) {
+      throw new AppError(
+        "Você precisa informar a senha antiga para definir uma nova senha!"
+      )
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if (!checkOldPassword) {
+        throw new AppError("A senha antiga não confere.")
+      }
+
+      user.password = await hash(password, 8)
+    }
 
     await database.run(
       `
     UPDATE users SET
     name = ?,
     email = ?,
-    updated_at = ?
+    password = ?,
+    updated_at = DATETIME('now')
     WHERE id = ?
     `,
-      [user.name, user.email, new Date(), id]
+      [user.name, user.email, user.password, id]
     )
 
     return response.json()
