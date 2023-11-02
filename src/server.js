504d96fa472ae0a0ca1a6aa85f6318dcffafa310
Rafael Loren175 <rafael.loren175@gmail.com>
2023-11-02 13:29:12 -0300

    added cors

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require("express-async-errors")
 
 const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
+
+const cors = require("cors")
 const express = require("express")
 const routes = require("./routes")
 const uploadConfig = require("./configs/upload")
@@ -9,6 +11,7 @@ const uploadConfig = require("./configs/upload")
 migrationsRun()
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
