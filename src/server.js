10f232cc17e830e69934fe5c64faf5a91d711631
Rafael Loren175 <rafael.loren175@gmail.com>
2023-12-19 11:59:28 -0300

    change port number

--- src/server.js
+++ src/server.js
@@ -35,5 +35,5 @@ app.use((error, request, response, next) => {
   })
 })
 
-const PORT = process.env.SERVER_PORT || 3000
+const PORT = process.env.SERVER_PORT || 3333
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`))
